ec01e0a15148916c4a561614eed089ec092112dc
Emil Olsen <emilberolsen@gmail.com>
2020-05-06 05:23:44 -0700

    f

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,32 @@
     <div class="Header">
       <span class="Logo"><i class="fas fa-gem"></i> CSGOSPIN</span>
       <div class="Menu">
-        <div class="menuAvatar"></div>
+        <div class="avatarWrapper">
+          <div class="menuAvatar"></div>
+        </div>
         <div class="menuName">{{User.Name}}</div>
         <div class="menuBalance"><i class="fas fa-coins"></i> {{User.Balance}}</div>
         <button class="menuBTN"><i class="fas fa-plus-circle"></i></button>
         <button class="menuBTN"><i class="fas fa-minus-circle"></i></button>
+        <button class="menuBTN"><i class="fas fa-headset"></i></button>
         <button class="menuBTN"><i class="fas fa-sign-out-alt"></i></button>
       </div>
     </div>
     <div class="gameModes">
-      <div class="gameMode" :class="{gameModeActive: $route.path === n.URL}" v-for="n in Gamemodes" :key="n" @click="changePage(n.URL)"><i class="fas fa-dice"></i> {{n.Name}}</div>
+      <div class="gameMode" :class="{gameModeActive: $route.path === n.URL}" v-for="(n, index) in Gamemodes" :key="n+index" @click="changePage(n.URL)"><i class="fas fa-dice"></i> {{n.Name}}</div>
     </div>
     <div class="Content">
       <div class="sidebarLeft">
         <div class="Chat">
           <div class="innerChat">
             <div class="chatMessage" v-for="(m, index) in messages" :key="m+index">
-              <div class="User">{{m.User}} <span class="postedTime"><i class="far fa-clock"></i> {{m.Posted}}</span></div>
+              <div class="User">
+                <div class="chatUserWrapper">
+                  <div class="chatAvatar"></div>
+                  {{m.User}}
+                  </div> 
+              <span class="postedTime"><i class="far fa-clock"></i> {{m.Posted}}</span>
+              </div>
               <div class="Message">{{m.Msg}}</div>
             </div>
           </div>
@@ -35,7 +44,9 @@
         </transition>
       </div>
     </div>
-    <div class="Footer"></div>
+    <div class="Footer">
+      Copyright © 2020 by CSGOSpin
+    </div>
   </div>
 </template>
 
@@ -56,7 +67,7 @@ export default {
       ],
       User: {
         Name: 'EMILO',
-        Balance: 835.34,
+        Balance: Math.floor(Math.random() * 1000) + 1 .toFixed(2),
         Avatar: ''
       }
     }
